d7870aa741364f3979639a8bd686d10094559e37
alphasourceai <jason@alphasourceai.com>
2025-07-25 11:08:42 -0600

    Cleaned up for Render deployment

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 import express from 'express';
 import multer from 'multer';
-import { initializeApp } from 'firebase-admin/app';
-import { getStorage } from 'firebase-admin/storage';
-import { getFirestore } from 'firebase-admin/firestore';
-import uploadRouter from './routes/upload.js';
 import dotenv from 'dotenv';
+import uploadRouter from './routes/upload.js';
 
 dotenv.config();
 
 const app = express();
 const upload = multer({ storage: multer.memoryStorage() });
-
-initializeApp({ storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com` });
 
 app.use('/upload-resume', upload.single('resume'), uploadRouter);
 
